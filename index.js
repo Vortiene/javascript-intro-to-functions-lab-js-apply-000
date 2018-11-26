d2766328d847262026ab0d10e1a5f975d29a15a9
pragmatic-expression-7847 <vortiene@gmail.com>
2018-11-26 18:50:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ function whisper(string) {
   return string.toLowerCase()
 }
 function logShout(string){
+<<<<<<< HEAD
   console.log(string.toUpperCase())
   
 }
@@ -15,4 +16,8 @@ function sayHiToGrandma(string){
   if (string === string.toLowerCase()) { return 'I can\'t hear you!'}
   if (string === string.toUpperCase()) {return 'YES INDEED!'}
   if (string === "I love you, Grandma.") {return "I love you, too."}
+=======
+  console.log(string.toUpperCase)
+  
+>>>>>>> 12cd3e461595c662cbc85db342747dc0a2e2fad0
 }
